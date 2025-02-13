5676030fb6abc5d43db1c83b5fbcf06fda9e4130
JuanC <jdiegocifuentes@ucundinamarca.edu.co>
2025-02-13 17:18:27 -0500

    salidas por consola

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        System.out.println("hola mundo");
+        System.out.println("hola mundo");
+        System.out.println("hola mundo");
+        System.out.println("hola mundo");
+        System.out.println("hola mundo");
+        System.out.println("hola mundo");
+        System.out.println("hola mundo");
 
 
     }
